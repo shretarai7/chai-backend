532b8ce0725b4b47533c54337524acdc5936ee07
shretarai7 <shretarai693@gmail.com>
2026-07-21 11:42:27 +0530

    multer and cloudinary js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+// It give a local file path that go on the server
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"  // fs is a file system help to read write and remove it mangages all the files
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+}); //confirgation gives us info about file creation deletion
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto" // koun sa resoucrce use kar rhe hai
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}
