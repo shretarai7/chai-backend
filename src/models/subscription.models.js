016ad5e1643c022f8b66b03253e6061b76f167e4
shretarai7 <shretarai693@gmail.com>
2026-08-16 15:53:10 +0530

    docs: enhance comments in subscription model for clarity on relationships and functionality

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -11,4 +11,9 @@ const subscriptionSchema = new Schema({
     }, 
 },  {timestamps: true})
 
+//user and channels both are user id
+//when we subsribe to a channel, we will create a new document in the subscription collection with subscriber and channel fields populated with the respective user ids.
+// same user can subscribe to multiple channels, and a channel can have multiple subscribers and a new subscription document is created for each subscription. This is a many-to-many relationship between users and channels.
+//when we count the number of subscribers for a channel, we will count the number of documents in the subscription collection where the channel field is equal to the channel's user id.
+
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
