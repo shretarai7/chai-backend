2a42f96c02804986f5f880d8ec07b178204923eb
shretarai7 <shretarai693@gmail.com>
2026-08-20 18:29:13 +0530

    feat: add user account management routes for password change, profile updates, and image uploads

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,14 @@ import {
     loginUser,
     logoutUser,
     registerUser,
-    refreshAccessToken
+    refreshAccessToken,
+    updateAccountDetails,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 } from "../controllers/user.controllers.js";
 
 import { upload } from "../middlewares/multer.middlewares.js";
@@ -31,6 +38,20 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage); //upload.single("coverImage") is used to upload a single file with the field name "coverImage" and this is a multer middleware that will handle the file upload and store it in the specified destination folder and then we can access the file in the controller using req.file
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //get user by username and this is also a params
+router.route("/watch-history").get(verifyJWT, getWatchHistory); //get user watch history 
+
+
+
 
 
 export default router;
